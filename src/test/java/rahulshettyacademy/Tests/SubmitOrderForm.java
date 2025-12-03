b5f4c53cded40cc11a17ff59837f33a28617c5bd
MayaGadagkar <gadagkarmaya@gmail.com>
2025-12-03 16:01:31 +0100

    few changes are made

--- src/test/java/rahulshettyacademy/Tests/SubmitOrderForm.java
+++ src/test/java/rahulshettyacademy/Tests/SubmitOrderForm.java
@@ -32,7 +32,7 @@ public class SubmitOrderForm extends BaseTest {
 		ConfirmationPage confirmationPage = paymentPage.AddDetailsAndPlaceOrder("India");
 		String msgConfirmation = confirmationPage.getConfirmation();
 		Assert.assertTrue(msgConfirmation.equalsIgnoreCase(expectedMsg));
-		System.out.println("Your order is placed");
+		System.out.println("Your order is placed successfully!!!");
 	}
 
 	@Test(dependsOnMethods = { "submitOrder" })
